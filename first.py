8597a003573a7e32be3ea16b262432fd92d31db7
Guruu-create <prasadrajarapu12@gmail.com>
2025-12-25 11:07:38 +0530

    initial commit

---
+++ first.py
@@ -0,0 +1,15 @@
+print("Hello World")
+print("Guru")
+print(2+6)
+
+
+first = 20
+second = 30
+third = first + second
+print(third)
+
+age = 18
+if age >= 18:
+    print("you can vote")
+else:
+    print("you cannot vote")
